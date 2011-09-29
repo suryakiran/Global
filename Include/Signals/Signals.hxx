5f1a3d64b123ccf5c602bcc2fd677c7715bc52b4
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2011-09-29 13:54:51 +0530

    Use CRTP instead of ugly macros for boost signal/slot support.

--- Include/Signals/Signals.hxx
+++ Include/Signals/Signals.hxx
@@ -6,33 +6,88 @@
 #include <set>
 using namespace std ;
 
-#include <boost/fusion/container.hpp>
-#include <boost/fusion/sequence/intrinsic.hpp>
-#include <boost/fusion/functional.hpp>
-#include <boost/fusion/iterator.hpp>
-#include <boost/fusion/include/for_each.hpp>
-namespace fusion = boost::fusion;
+#include <Signals/signal_types.h>
 
+#include <boost/type_traits/remove_reference.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/signals2.hpp>
 
+#include <boost/preprocessor/control/expr_if.hpp>
+#include <boost/preprocessor/arithmetic/inc.hpp>
+#include <boost/preprocessor/iteration/local.hpp>
+#include <boost/preprocessor/punctuation/comma_if.hpp>
+#include <boost/preprocessor/repetition/enum_shifted_binary_params.hpp>
+#include <boost/preprocessor/repetition/enum_shifted_params.hpp>
+
+#ifndef MAX_SIGNAL_PARAMS
+#define MAX_SIGNAL_PARAMS 10
+#endif 
+
 #define BOOST_SIGNAL(...) boost::shared_ptr< boost::signals2::signal < __VA_ARGS__ > >
 
+template <class derived_class>
 class SignalBase
 {
-	public:
-		~SignalBase () { }
+  public:
+    typedef typename signal::types<derived_class>::type signal_type;
+    typedef SignalBase<derived_class> base_type;
 
-		bool blockSignals (void)         { return m_blockSignals ; }
+  private:
+    template<class T> 
+      struct get_type                                                   
+      {                                                                                  
+        typedef typename boost::remove_reference <
+          typename fusion::result_of::at_key<signal_type, T>::type>::type
+          ::reference value_type;
+      };                                                                                
+
+    struct initialize                                                                  
+    {                                                                                  
+      template <typename FusionMapItem>
+        void operator() (FusionMapItem& p_item) const
+        {
+          typedef typename FusionMapItem::second_type SmartPtr;
+          typedef typename SmartPtr::value_type ItemType ;
+          p_item.second = SmartPtr(new ItemType) ;
+        }
+    };                                                                               
+
+  public:
+    SignalBase()
+      :m_blockSignals(false)
+    {
+      fusion::for_each (m_signals, initialize());
+    }
+
+    template <class T>                                                                  
+      typename get_type<T>::value_type                                              
+      signal (void) { return *(fusion::at_key<T>(m_signals)) ; }           
+
+    virtual ~SignalBase()
+    {
+    }
+
+		bool blockSignals (void)   { return m_blockSignals ; }
 		void blockSignals (bool b) { m_blockSignals = b ; }
 
-	protected:
-    SignalBase () 
-      : m_blockSignals (false) { }
+#define BOOST_PP_LOCAL_MACRO(n)\
+	template<class id BOOST_PP_COMMA_IF(n) BOOST_PP_ENUM_SHIFTED_PARAMS(BOOST_PP_INC(n), typename T)> \
+	void emitSignal(BOOST_PP_IF(n,BOOST_PP_ENUM_SHIFTED_BINARY_PARAMS,void BOOST_PP_TUPLE_EAT(3))\
+			(BOOST_PP_INC(n), const T, &var))\
+	{\
+		fusion::fused<typename get_type<id>::value_type> functionObj (signal<id>()) ;\
+		if (blockSignals()) return ;\
+		functionObj (fusion::make_vector(BOOST_PP_ENUM_SHIFTED_PARAMS(BOOST_PP_INC(n), var))) ;\
+	}
 
-	private:
+#define BOOST_PP_LOCAL_LIMITS (0, MAX_SIGNAL_PARAMS)
+#include BOOST_PP_LOCAL_ITERATE()
+
+  protected:
+    signal_type m_signals;
+
+  private:
 		bool m_blockSignals ;
-} ;
-
+};
 #endif
 
---
+++ Include/Signals/signal_types.h
@@ -0,0 +1,15 @@
+#ifndef signal_types_h_INCLUDED
+#define signal_types_h_INCLUDED
+
+#include <boost/fusion/container.hpp>
+#include <boost/fusion/sequence/intrinsic.hpp>
+#include <boost/fusion/functional.hpp>
+#include <boost/fusion/iterator.hpp>
+#include <boost/fusion/include/for_each.hpp>
+namespace fusion = boost::fusion;
+
+namespace signal {
+  template <class T> struct types;
+}
+
+#endif
