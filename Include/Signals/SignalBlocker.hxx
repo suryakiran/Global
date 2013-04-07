b74e58d081b30f8f2acddf27da05d0a8936eca64
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2013-04-07 14:24:34 +0530

    Signal Blocker currently handles only qt signals

--- Include/Signals/SignalBlocker.hxx
+++ Include/Signals/SignalBlocker.hxx
@@ -1,38 +1,27 @@
 #ifndef SignalBlocker_hxx_INCLUDED
 #define SignalBlocker_hxx_INCLUDED
 
+#include <QObject>
+
 class SignalBlocker
 {
-  public:
-    SignalBlocker (SignalBase* p_object)
-      : m_boostObject(p_object), m_origSetting(p_object->blockSignals())
+public:
+  SignalBlocker (QObject* p_object)
+    : m_qtObject(p_object), m_origSetting(p_object->signalsBlocked())
     {
-      m_qtObject = 0;
     }
 
-    SignalBlocker (QObject* p_object)
-      : m_qtObject(p_object), m_origSetting(p_object->blockSignals())
+  ~SignalBlocker()
     {
-      m_boostObject = 0;
-    }
-
-    ~SignalBlocker()
-    {
-      if (m_boostObject)
-      {
-        m_boostObject->blockSignals(m_origSetting);
-      }
-      else if (m_qtObject)
+      if (m_qtObject)
       {
         m_qtObject->blockSignals(m_origSetting);
       }
     }
 
-  private:
-    SignalBase* m_boostObject;
-    QObject* m_qtObject;
-
-    bool m_origSetting;
-}
+private:
+  QObject* m_qtObject;
+  bool m_origSetting;
+};
 
 #endif
